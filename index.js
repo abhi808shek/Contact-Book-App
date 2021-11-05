6c7609771a3a8dae57768eda717af490dd14a3dd
abhi808shek <abhi808gupta@gmail.com>
2021-11-05 22:09:40 +0530

    Change Folder Structure and done deployment setup

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/api/",route);
+app.use(express.static("client/build"));
 
 const PORT = process.env.PORT || 8000;
 
-if (process.env.NODE_ENV == "production") {
-    app.use(express.static("client/build"));
-}
+
+
 
 app.listen(PORT,()=>{
     console.log("Server Conneted Successfully...");
