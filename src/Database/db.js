c37d6dbd35aa59479eed4beb3611d58c809b12a6
abhi808shek <abhi808gupta@gmail.com>
2021-11-06 00:46:13 +0530

    changin database url

--- src/Database/db.js
+++ src/Database/db.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose");
 require('dotenv').config()
-const URL = process.env.DB_URL;
-
 
 const dataBaseConnection = async()=>{
 
   try {
-    await  mongoose.connect(URL)
+    await  mongoose.connect(process.env.DB_URL)
     console.log("DataBase Connected Successfullyy....");
   } catch (error) {
     console.log(error.message);
