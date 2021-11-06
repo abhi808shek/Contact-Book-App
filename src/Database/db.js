2bc349200b888507330f00266ca26510a60d3c99
abhi808shek <abhi808gupta@gmail.com>
2021-11-06 16:55:07 +0530

    Database file updated

--- src/Database/db.js
+++ src/Database/db.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 require('dotenv').config()
 
+const URI = process.env.DB_URL;
+
 const dataBaseConnection = async()=>{
 
   try {
-    await  mongoose.connect(process.env.DB_URL)
+    await  mongoose.connect(URI)
     console.log("DataBase Connected Successfullyy....");
   } catch (error) {
     console.log(error.message);
