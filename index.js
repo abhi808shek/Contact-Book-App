898dc03be6f5e88e71d9770c6d529476b3ae1fcc
abhi808shek <abhi808gupta@gmail.com>
2021-11-06 11:30:27 +0530

    file modified

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const route = require("./src/Routers/Api");
-// const path = require('path');
+const path = require('path');
 
 require("./src/Database/db")
 require('dotenv').config()
@@ -16,10 +16,10 @@ const PORT = process.env.PORT || 8000;
 
 if(process.env.NODE_ENV === 'production'){
     //set static folder
-    app.use(express.static('public/build'));
+    app.use(express.static('client/build'));
 }
 app.get('*',(req, res) => {
-    res.sendFile(path.resolve(__dirname, 'public', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
 
