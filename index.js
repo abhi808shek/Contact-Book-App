38b9b7763ff5389783b2fb285b5120f8a41397ec
abhi808shek <abhi808gupta@gmail.com>
2021-11-06 11:19:21 +0530

    Index.js file modified

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ if(process.env.NODE_ENV === 'production'){
     //set static folder
     app.use(express.static('public/build'));
 }
-// app.get('*',(req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'public', 'build', 'index.html'));
-// });
+app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'build', 'index.html'));
+});
 
 
 app.listen(PORT,()=>{
